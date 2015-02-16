0b2d7e39c6d051c7dece697c071ca9a06453b993
Jirka Lewandowski <jirka.lewandowski@wzb.eu>
2015-02-16 15:17:44 +0100

    More tests for corpus and cache

--- tests/testthat/testcorpus.R
+++ tests/testthat/testcorpus.R
@@ -1,15 +1,52 @@
 manifestodb.setapikey(key.file = "../manifesto_apikey.txt")
 
 manifesto.emptycache()
-mpds <- manifesto.maindataset()
-swe <- subset(mpds, countryname == "Sweden")
-corp <- manifesto.corpus(swe)
 
-skn <- subset(mpds, countryname == "Sweden" | countryname == "Norway")
-corp2 <- manifesto.corpus(skn)
+cache_vname <- function(party, date, manifesto_id) {
+  paste(ktextname, party, date, manifesto_id, sep = "_")
+}
 
-nor <- subset(mpds, countryname == "Norway")
-corp3 <- manifesto.corpus(nor)
+check_cache_mapping <- function(doc) {
+  vname <- cache_vname(meta(doc, "party"), meta(doc, "date"), meta(doc, "manifesto_id"))
+  cachedoc <- get(vname, envir = mp_cache)
+  expect_equal(head(cachedoc$items[[1]]$content), head(content(doc)))
+}
 
-head(as.data.frame(corp2))
-head(as.data.frame(corp2, with.meta = TRUE))
+test_that("simple subset corpus download works", {
+  
+  mpds <- manifesto.maindataset()
+  swe <- subset(mpds, countryname == "Sweden")
+  corp <- manifesto.corpus(swe)
+  
+  expect_is(corp, c("ManifestoCorpus", "Corpus"))
+  expect_more_than(length(corp), 10)
+})
+
+test_that("caching of corpus works correctly", {
+  
+  mpds <- manifesto.maindataset()
+  swe <- subset(mpds, countryname == "Sweden")
+  corpswe <- manifesto.corpus(swe)
+  
+  ## check that corpus parts are now in cache
+  corpids <- manifesto.meta(subset(manifesto.availability(swe)$availability, annotations))
+  vnames <- cache_vname(corpids$party, corpids$date, corpids$manifesto_id)
+  expect_true(all(exists(vnames, envir = mp_cache)))
+  
+  ## check that mapping of ids and texts is the same in corpus and cache
+  lapply(content(corpswe), check_cache_mapping)
+  corpnsw <- manifesto.corpus(subset(mpds, countryname %in% c("Sweden", "Norway")))
+  lapply(content(corpnsw), check_cache_mapping)
+  
+})
+
+## more checks with one specific test document ?
+
+# skn <- subset(mpds, countryname == "Sweden" | countryname == "Norway")
+# corp2 <- manifesto.corpus(skn)
+# 
+# nor <- subset(mpds, countryname == "Norway")
+# corp3 <- manifesto.corpus(nor)
+# 
+# head(as.data.frame(corp2))
+# head(as.data.frame(corp2, with.meta = TRUE))
