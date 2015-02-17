246cc45b4a50043b682e9af3830aecb77d37fde9
Jirka Lewandowski <jirka.lewandowski@wzb.eu>
2015-02-17 18:28:00 +0100

    Scaling functions applicable to ManifestoCorpus and ManifestoDocument
    logit_rile currently disabled

--- R/scaling.R
+++ R/scaling.R
@@ -81,33 +81,145 @@ create.scaling <- function(pos, neg,
         ...)
 }
 
-rile.r <- c(104, 201, 203, 305, 401, 402, 407, 414, 505, 601, 603, 605, 606)
-rile.l <- c(103, 105, 106, 107, 202, 403, 404, 406, 412, 413, 504, 506, 701)
+rile_r <- c(104, 201, 203, 305, 401, 402, 407, 414, 505, 601, 603, 605, 606)
+rile_l <- c(103, 105, 106, 107, 202, 403, 404, 406, 412, 413, 504, 506, 701)
+
+table_to_df <- function(tt, prefix = "per", relative = TRUE) {
+  df <- as.data.frame(t(as.matrix(tt)))
+  names(df) <- paste0(prefix, names(df))
+  if (relative) {
+    n <- sum(df[1,])
+    df[1,] <- df[1,]/n
+    df$total <- n
+  }
+  return(df)
+}
+
+#' Make a scaling function applicable to a ManifestoDocument
+#'
+#' @param scalingfun a scaling function, taking a data.frame with variables
+#' named "per..." and returning a scaling measure (such as the rile) in a vector.
+#' @param returndf if this flag is TRUE, a data.frame with category percentage values,
+#' scaling result and, if available party and date is returned by the returned function
+#' @return a function that takes a ManifestoDocument and computes the scaled value
+#' for it
+#' 
+#' @export
+#' @rdname corpus_scaling
+document_scaling <- function(scalingfun, returndf = FALSE, scalingname = "scaling") {
+  
+  return(function(doc) {
+  
+    df <- data.frame(party=meta(doc, "party"), date=meta(doc, "date"))
+    df <- bind_cols(df, table_to_df(table(codes(doc))))
+
+    df[,scalingname] <- scalingfun(df)
+    
+    if (returndf) {
+      return(df)
+    } else {
+      return(df[1,scalingname])
+    }
+    
+  })
+}
+
+#' Make a scaling function applicable to a ManifestoCorpus
+#'
+#' @param scalingfun a scaling function, taking a data.frame with variables
+#' named "per..." and returning a scaling measure (such as the rile) in a vector.
+#' @return a function that takes a ManifestoCorpus and returns a data.frame with
+#' the code percentages and the scaling function value (e.g. rile) for each document
+#' in the corpus
+#' 
+#' @export
+#' @rdname corpus_scaling
+corpus_scaling <- function(scalingfun, scalingname = "scaling") {
+  function(corpus) {
+    df <- data.frame(party = unlist(lapply(content(corpus), function(doc) { meta(doc, "party")})),
+               date = unlist(lapply(content(corpus), function(doc) { meta(doc, "date")})),
+               scaling = unlist(lapply(content(corpus), document_scaling(scalingfun))))
+    names(df)[3] <- scalingname
+    df
+  }
+}
 
 #' RILE
 #' 
-#' Computes the RILE or other bipolar linear scaling measures for each case in a data.frame
+#' Computes the RILE or other bipolar linear scaling measures for each case in a
+#' data.frame or ManifestoCorpus
 #'
 #' @param data A data.frame with cases to be scaled, variables named "per..."
 #' @export
-rile <- create.scaling(pos=rile.r, neg=rile.l)
+rile <- function(x) { UseMethod("rile", x) }
+#' @rdname rile
+#' @export
+rile.default <- create.scaling(pos=rile_r, neg=rile_l)
+#' @rdname rile
+#' @export
+rile.ManifestoDocument <- document_scaling(rile.default, scalingname = "rile")
+#' @rdname rile
+#' @export
+rile.ManifestoCorpus <- corpus_scaling(rile.default, scalingname = "rile")
+
+#' @rdname rile
+logit_rile <- function(x) { UseMethod("logit_rile", x) }
+#' @rdname rile
+logit_rile.default <- create.scaling(pos=rile_r, neg=rile_l, base.fun=logit.scaling)
+#' @rdname rile
+logit_rile.ManifestoDocument <- document_scaling(logit_rile.default, scalingname = "logit_rile")
+#' @rdname rile
+logit_rile.ManifestoCorpus <- corpus_scaling(logit_rile.default, scalingname = "logit_rile")
+
 
 #' @rdname rile
 #' @export
-logit.rile <- create.scaling(pos=rile.r, neg=rile.l, base.fun=logit.scaling)
+planeco <- function(x) { UseMethod("planeco", x) }
+#' @rdname rile
+#' @export
+planeco.default <- create.scaling(pos=c(403, 404, 412), neg=c())
+#' @rdname rile
+#' @export
+planeco.ManifestoDocument <- document_scaling(planeco.default, scalingname = "planeco")
+#' @rdname rile
+#' @export
+planeco.ManifestoCorpus <- corpus_scaling(planeco.default, scalingname = "planeco")
 
 #' @rdname rile
 #' @export
-planeco <- create.scaling(pos=c(403, 404, 412), neg=c())
+markeco <- function(x) { UseMethod("markeco", x) }
+#' @rdname rile
+#' @export
+markeco.default <- create.scaling(pos=c(401, 414), neg=c())
+#' @rdname rile
+#' @export
+markeco.ManifestoDocument <- document_scaling(markeco.default, scalingname = "markeco")
+#' @rdname rile
+#' @export
+markeco.ManifestoCorpus <- corpus_scaling(markeco.default, scalingname = "markeco")
 
 #' @rdname rile
 #' @export
-markeco <- create.scaling(pos=c(401, 414), neg=c())
+welfare <- function(x) { UseMethod("welfare", x) }
+#' @rdname rile
+#' @export
+welfare.default <- create.scaling(pos=c(503, 504), neg=c())
+#' @rdname rile
+#' @export
+welfare.ManifestoDocument <- document_scaling(welfare.default, scalingname = "welfare")
+#' @rdname rile
+#' @export
+welfare.ManifestoCorpus <- corpus_scaling(welfare.default, scalingname = "welfare")
 
 #' @rdname rile
 #' @export
-welfare <- create.scaling(pos=c(503, 504), neg=c())
-
+intpeace <- function(x) { UseMethod("intpeace", x) }
 #' @rdname rile
 #' @export
-intpeace <- create.scaling(pos=c(102, 105, 106), neg=c())
+intpeace.default <- create.scaling(pos=c(102, 105, 106), neg=c())
+#' @rdname rile
+#' @export
+intpeace.ManifestoDocument <- document_scaling(intpeace.default, scalingname = "intpeace")
+#' @rdname rile
+#' @export
+intpeace.ManifestoCorpus <- corpus_scaling(intpeace.default, scalingname = "intpeace")
--- tests/testthat/testscaling.R
+++ tests/testthat/testscaling.R
@@ -11,6 +11,33 @@ test_that("rile computation from dataset equals dataset value", {
   
 })
 
+scaling_as_expected <- function(corp, scalingfun, scalingname) {
+  
+  scale1 <- scalingfun(corp[[1]])
+  expect_is(scale1, "numeric")
+  expect_false(is.na(scale1))
+  
+  scale_corp <- scalingfun(corp)
+  expect_is(scale_corp, "data.frame")
+  expect_true(all(c("party", "date", scalingname) %in% names(scale_corp)))
+  expect_false(any(is.na(scale_corp[,scalingname])))
+  expect_equal(nrow(scale_corp), length(corp))
+}
+
+test_that("corpus and document scaling works", {
+  
+  corp <- manifesto.corpus(mpds.fr)
+
+  scaling_as_expected(corp, rile, "rile")
+#   scaling_as_expected(corp, logit_rile, "logit_rile")
+  scaling_as_expected(corp, planeco, "planeco")
+  scaling_as_expected(corp, markeco, "markeco")
+  scaling_as_expected(corp, intpeace, "intpeace")
+  scaling_as_expected(corp, welfare, "welfare")
+  
+})
+
+
 ## TODO more tests, of other functions
 
 # comparedf$logit.rile <- logit.rile(mpds.fr)
