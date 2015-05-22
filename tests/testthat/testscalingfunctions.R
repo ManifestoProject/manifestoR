8c66d419f56be5a1b55ddd8d53f59ee057e7dd1a
Jirka Lewandowski <jirka.lewandowski@wzb.eu>
2015-05-22 15:26:29 +0200

    Make vanilla scaling test compatible with MPDS 2015a format

--- tests/testthat/testscalingfunctions.R
+++ tests/testthat/testscalingfunctions.R
@@ -5,17 +5,13 @@ mpds <- mp_maindataset()
 
 test_that("Vanilla scaling produces no error", {
 
-  westeurope <- filter(mpds, country<70) %>% 
+  allpers <- filter(mpds, country<70) %>% 
     filter(date > 198000) %>%
-    select(1:76,rile)
-
-  allpers <- select(westeurope, starts_with("per")) %>%
-    select(-pervote) %>%
-    select(1:56)
+    select(matches("(^per(\\d{3}|(uncod))$)|(rile)"))
 
   ### vanilla test
 
-  westeurope$vanilla.inv <- vanilla(westeurope, invert=1)
-  westeurope$vanilla <- vanilla(westeurope, invert=0)
+  westeurope$vanilla.inv <- vanilla(allpers, invert=1)
+  westeurope$vanilla <- vanilla(allpers, invert=0)
 
 })
